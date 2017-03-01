2dbdd43211b6e39480fcc15728439d3114a4ac35
ivan_nikolaev <60277@mail.ru>
2017-03-01 09:35:51 +0500

    first commit

---
+++ happy_ticket/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace happy_ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                a++;
+            }
+        }
+    }
+}
